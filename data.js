9238c8e8822d242230765bd042bea6dd3b32ea3b
ramees2415 <rameesck6937@gmail.com>
2026-02-01 00:41:11 +0530

    Add files via upload

---
+++ data.js
@@ -0,0 +1,831 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-exterior",
+      "name": "exterior",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.593687123733103,
+          "pitch": 0.31437192418326276,
+          "rotation": 0,
+          "target": "2-pharmacy-exterior"
+        },
+        {
+          "yaw": -0.07310492598073814,
+          "pitch": 0.40219908007199834,
+          "rotation": 0,
+          "target": "1-entry"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "1-entry",
+      "name": "entry",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.012790040578593675,
+          "pitch": 0.37570883990451165,
+          "rotation": 23.561944901923464,
+          "target": "17-recaption"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "2-pharmacy-exterior",
+      "name": "pharmacy exterior",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.2594745356353538,
+          "pitch": 0.17071111834299124,
+          "rotation": 0,
+          "target": "1-entry"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "3-doctor-cabin",
+      "name": "doctor cabin",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.6433237399276166,
+          "pitch": 0.47078889321393547,
+          "rotation": 0,
+          "target": "13-physiotherapy"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "4-physiotherapy",
+      "name": "physiotherapy",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.2700668851400714,
+          "pitch": 0.46166251729848895,
+          "rotation": 0,
+          "target": "13-physiotherapy"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "5-lift",
+      "name": "lift",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.6994110126918613,
+          "pitch": 0.08376319316369774,
+          "rotation": 0,
+          "target": "6-stair"
+        },
+        {
+          "yaw": -0.6549687465582181,
+          "pitch": 0.36276980665661895,
+          "rotation": 4.71238898038469,
+          "target": "3-doctor-cabin"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "6-stair",
+      "name": "stair",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.9358715280804724,
+          "pitch": 0.5570247603487246,
+          "rotation": 7.853981633974483,
+          "target": "15-conferance-room"
+        },
+        {
+          "yaw": -2.291523124685842,
+          "pitch": 0.3093158681114403,
+          "rotation": 4.71238898038469,
+          "target": "14-pharmacy"
+        },
+        {
+          "yaw": -2.205065600573949,
+          "pitch": 0.2546090237603966,
+          "rotation": 0,
+          "target": "16-lab"
+        },
+        {
+          "yaw": 0.8592315231530616,
+          "pitch": 0.18419931277687596,
+          "rotation": 0,
+          "target": "5-lift"
+        },
+        {
+          "yaw": -1.6136981006759417,
+          "pitch": 0.34724611237870384,
+          "rotation": 0,
+          "target": "12-naures-station"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "7-duplex-0",
+      "name": "duplex 0",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.12383887180670605,
+          "pitch": 0.17649064923876168,
+          "rotation": 4.71238898038469,
+          "target": "8-cafa"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "8-cafa",
+      "name": "cafa",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.829758533319069,
+          "pitch": 0.06237985236593957,
+          "rotation": 0,
+          "target": "9-seating"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "9-seating",
+      "name": "seating",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.6758931336227825,
+          "pitch": 0.05079028944197361,
+          "rotation": 0,
+          "target": "8-cafa"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "10-duplex-",
+      "name": "duplex ",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.008349766495676647,
+          "pitch": 0.23847953825936763,
+          "rotation": 0,
+          "target": "7-duplex-0"
+        },
+        {
+          "yaw": -0.5599521270513144,
+          "pitch": 0.3898430916316329,
+          "rotation": 0,
+          "target": "19-kitchen"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "11-bed-room",
+      "name": "bed room",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.5878640787323732,
+          "pitch": 0.13025148788749874,
+          "rotation": 0,
+          "target": "12-naures-station"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "12-naures-station",
+      "name": "naures station",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.3287208916382065,
+          "pitch": 0.08150843589510615,
+          "rotation": 4.71238898038469,
+          "target": "11-bed-room"
+        },
+        {
+          "yaw": 2.925930754439583,
+          "pitch": 0.03724233969002633,
+          "rotation": 4.71238898038469,
+          "target": "10-duplex-"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "13-physiotherapy",
+      "name": "physiotherapy",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.7765368215886301,
+          "pitch": 0.36823617808318687,
+          "rotation": 3.9269908169872414,
+          "target": "4-physiotherapy"
+        },
+        {
+          "yaw": 0.3425876541112274,
+          "pitch": 0.1638767972889852,
+          "rotation": 0,
+          "target": "18-patient-room"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "14-pharmacy",
+      "name": "pharmacy",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.749659881404032,
+          "pitch": 1.002829621682272,
+          "rotation": 5.497787143782138,
+          "target": "6-stair"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "15-conferance-room",
+      "name": "conferance room",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.4923324480222746,
+          "pitch": 0.5635680568065524,
+          "rotation": 4.71238898038469,
+          "target": "6-stair"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "16-lab",
+      "name": "lab",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.4127105951896137,
+          "pitch": 0.37189764151061766,
+          "rotation": 0,
+          "target": "6-stair"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "17-recaption",
+      "name": "recaption",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.2697648013568017,
+          "pitch": 0.2684811217008125,
+          "rotation": 4.71238898038469,
+          "target": "5-lift"
+        },
+        {
+          "yaw": -0.6731593997282275,
+          "pitch": 0.28467287352370896,
+          "rotation": 0,
+          "target": "3-doctor-cabin"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "18-patient-room",
+      "name": "patient room",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.4558480811009797,
+          "pitch": 0.13344982879486977,
+          "rotation": 0,
+          "target": "6-stair"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "19-kitchen",
+      "name": "kitchen",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.5633228374854795,
+          "pitch": 1.3813477585476015,
+          "rotation": 4.71238898038469,
+          "target": "7-duplex-0"
+        }
+      ],
+      "infoHotspots": []
+    }
+  ],
+  "name": "Value First",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": true,
+    "viewControlButtons": true
+  }
+};
